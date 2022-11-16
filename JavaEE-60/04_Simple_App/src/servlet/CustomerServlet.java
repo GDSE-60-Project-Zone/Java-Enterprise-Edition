c254a396c39de16b4539618f1468bf757425665c
sanuv9683 <sanuv9683@gmail.com>
2022-11-16 09:55:35 +0530

    Content Updated

--- JavaEE-60/04_Simple_App/src/servlet/CustomerServlet.java
+++ JavaEE-60/04_Simple_App/src/servlet/CustomerServlet.java
@@ -1,4 +1,22 @@
 package servlet;
 
-public class CustomerServlet {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/customer")
+public class CustomerServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String id = req.getParameter("id");
+        String name = req.getParameter("name");
+        String address = req.getParameter("address");
+        String salary = req.getParameter("salary");
+
+        System.out.println(id+" "+name+" "+address+" "+salary);
+    }
 }
