e6dee804d68bdafcfa9beb93908906435c6b6662
sanuv9683 <sanuv9683@gmail.com>
2022-11-25 14:11:33 +0530

    Content Updated

--- JavaEE-60/05_Mapping_Spec/src/servlet/ExtensionMapping.java
+++ JavaEE-60/05_Mapping_Spec/src/servlet/ExtensionMapping.java
@@ -7,11 +7,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-//Empty String Mapping
-@WebServlet(urlPatterns = "") //localhost:8080/appname
-public class EmptyStringMapping extends HttpServlet {
+@WebServlet(urlPatterns = "*.jpg") // Extension Mapping
+public class ExtensionMapping extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().write("Empty String Mapping");
+        resp.getWriter().write("Extension Mapping Invoked");
     }
 }
