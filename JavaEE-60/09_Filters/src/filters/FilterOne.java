6edc49e680ccb9d817dd4d22c9dfc942914156c2
sanuv9683 <sanuv9683@gmail.com>
2022-12-16 10:23:35 +0530

    First Filter Intrduced

---
+++ JavaEE-60/09_Filters/src/filters/FilterOne.java
@@ -0,0 +1,23 @@
+package filters;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+@WebFilter(urlPatterns = "/a")
+public class FilterOne implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        System.out.println("Filter One Initialized");
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        System.out.println("Filter One do Filter Method Invoked");
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("Filter One Destroyed");
+    }
+}
