56f4c35675f576c0ed16f88115611a4117053c09
sanuv9683 <sanuv9683@gmail.com>
2022-12-16 10:39:59 +0530

    Content Updated

---
+++ JavaEE-60/09_Filters/src/filters/FilterTwo.java
@@ -0,0 +1,23 @@
+package filters;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+@WebFilter(urlPatterns = "/b")
+public class FilterTwo implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        System.out.println("Filter Two Initialized");
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        System.out.println("Filter Two do Filter Method Invoked");
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("Filter Two Destroyed");
+    }
+}
