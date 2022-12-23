3d1b535745c13c50823c5bccde36ffb578baa00c
sanuv9683 <sanuv9683@gmail.com>
2022-12-23 10:58:44 +0530

    Content Updated

--- JavaEE-60/12_Listeners/src/listerns/MyListener.java
+++ JavaEE-60/12_Listeners/src/listerns/MyListener.java
@@ -1,5 +1,6 @@
 package listerns;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
@@ -9,6 +10,8 @@ public class MyListener implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
         System.out.println("Servlet Context Created");
+        ServletContext servletContext = servletContextEvent.getServletContext();
+        servletContext.setAttribute("pkey","PASAN");
     }
 
     @Override
